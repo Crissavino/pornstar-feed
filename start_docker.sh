3359393a203c441660f499c553e5cc70c16bd46e
cristiansavino <savinocristian89@gmail.com>
2023-05-15 21:31:43 +0300

    change name

--- start_docker.sh
+++ start_docker.sh
@@ -16,32 +16,32 @@ docker compose up -d
 # wait for the database to be ready
 sleep 5
 echo "Running composer install and composer key generate"
-docker exec -it pornstar_feed_app bash -c "composer install && php artisan key:generate"
+docker exec -it mindgeektest_app bash -c "composer install && php artisan key:generate"
 echo "Finish running composer install and composer key generate"
 
 echo "Running the migrations"
-docker exec -it --env="DB_HOST=pornstar_feed_db" --env="DB_PORT=3306" --env="DB_DATABASE=pornstar_feed" --env="DB_USERNAME=root" --env="DB_PASSWORD=root" pornstar_feed_app php artisan migrate:fresh --force
+docker exec -it --env="DB_HOST=mindgeektest_db" --env="DB_PORT=3306" --env="DB_DATABASE=mindgeektest" --env="DB_USERNAME=root" --env="DB_PASSWORD=root" mindgeektest_app php artisan migrate:fresh --force
 sleep 5
 echo "Finish running the migrations"
 
 echo "Running Tests"
-docker exec -it pornstar_feed_app php artisan test --testsuite=Feature --stop-on-failure
+docker exec -it mindgeektest_app php artisan test --testsuite=Feature --stop-on-failure
 sleep 5
 echo "Finish running tests"
 
 echo "Running the feed download command"
-docker exec -it pornstar_feed_app bash -c "php artisan feed:download"
+docker exec -it mindgeektest_app bash -c "php artisan feed:download"
 echo "Finish running the feed download command"
 
 echo "Starting the queue worker"
-docker exec -it pornstar_feed_app service supervisor start
-docker exec -it pornstar_feed_app supervisorctl reread
-docker exec -it pornstar_feed_app supervisorctl update
-docker exec -it pornstar_feed_app supervisorctl start queue-worker:*
+docker exec -it mindgeektest_app service supervisor start
+docker exec -it mindgeektest_app supervisorctl reread
+docker exec -it mindgeektest_app supervisorctl update
+docker exec -it mindgeektest_app supervisorctl start queue-worker:*
 echo "Finish starting the queue worker"
 
 echo "Start the cron job"
-docker exec -it pornstar_feed_app service cron start
+docker exec -it mindgeektest_app service cron start
 echo "Finish starting the cron job"
 
 echo "You can access the application at http://localhost:80"
